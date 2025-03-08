5d841fb71a85f202f6b11f1450c5538d46ec0175
T.L. Nimasha Shehani <nimashashehani0715@gmail.com>
2025-03-08 10:21:42 +0530

    first commit

---
+++ src/main/java/com/example/homeease/HomeEaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeEaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeEaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homeease/HomeEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeEaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
